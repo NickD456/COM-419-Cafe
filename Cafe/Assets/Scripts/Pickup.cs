e643a488eb39cb44e5358eb5678c1571dbf8c60d
flip4567 <147349946+flip4567@users.noreply.github.com>
2025-02-14 10:48:37 -0500

    added pickup UI and drop item ability

--- Cafe/Assets/Scripts/Pickup.cs
+++ Cafe/Assets/Scripts/Pickup.cs
@@ -8,11 +8,14 @@ public class Pickup : MonoBehaviour
     public GameObject hands;
     private GameObject item;
     private GameObject heldItem;
+    public GameObject pickUpText;
+    private bool interact;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         canpickup = false;    //setting both to false
         hasItem = false;
+        interact = false;
     }
 
     // Update is called once per frame
@@ -22,11 +25,29 @@ public class Pickup : MonoBehaviour
         {
             if (Input.GetKeyDown("e"))
             {
-                heldItem = Instantiate(item, hands.transform.position, Quaternion.identity);
-                heldItem.transform.SetParent(hands.transform);
-                heldItem.transform.rotation = item.transform.rotation;
+                if (!hasItem)
+                {
+                    heldItem = Instantiate(item, hands.transform.position, Quaternion.identity);
+                    heldItem.transform.SetParent(hands.transform);
+                    heldItem.transform.rotation = item.transform.rotation;
+                    hasItem = true;
+                }
+
+               
+
             }
         }
+        else 
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                if (hasItem && !interact)
+                {
+                    DropObject();
+                }
+            }
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -36,12 +57,25 @@ public class Pickup : MonoBehaviour
             canpickup = true;
             item = other.gameObject;
             Debug.Log("entered");
+            pickUpText.SetActive(true);
+        }
+
+        if (other.gameObject.tag == "machine")
+        {
+            interact = true;
         }
         
     }
     private void OnTriggerExit(Collider other)
     {
         canpickup = false; //when you leave the collider set the canpickup bool to false
+        interact = false;
+        pickUpText.SetActive(false);
+    }
 
+    private void DropObject()
+    {
+        Destroy(heldItem);
+        hasItem = false;
     }
 }
