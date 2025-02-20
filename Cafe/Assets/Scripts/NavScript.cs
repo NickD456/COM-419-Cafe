f032c336314884c76126da8b24f1f6822915b31d
flip4567 <147349946+flip4567@users.noreply.github.com>
2025-02-20 18:41:58 -0500

    working on customers

--- Cafe/Assets/Scripts/NavScript.cs
+++ Cafe/Assets/Scripts/NavScript.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavScript : MonoBehaviour
 {
+  
+    private NavMeshAgent agent;
+    private GameObject des;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        des = GameObject.FindWithTag("Line Spot");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = des.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Customer") && other.gameObject.GetInstanceID() != gameObject.GetInstanceID())
+        {
+            if (other.transform.position.z > transform.position.z)
+            {
+                agent.isStopped = true;
+            }
+        }
     }
 }
