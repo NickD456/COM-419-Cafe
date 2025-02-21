8cec0e8c4e2808f72d5e09102eae3515d202d0d4
Piechoc2004 <piechoc@sunypoly.edu>
2025-02-20 21:25:08 -0500

    Scene change and bug fixes
    Made enemies target player
    enemies die when shot
    when all zombies dead should change scene

--- Cafe/Assets/Chris_Script/ZombieAI.cs
+++ Cafe/Assets/Chris_Script/ZombieAI.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieAI : MonoBehaviour
 {
-    public GameObject Target;
+    //public GameObject Target;
     public float speed = 1.5f;
+
+    //public Transform player;
+    private NavMeshAgent agent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        transform.LookAt(Target.transform);
+        transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform);
         transform.Translate(Vector3.forward *Time.deltaTime * speed);
+
+
+       //transform.LookAt(Target.transform);
+       //transform.Translate(Vector3.forward *Time.deltaTime * speed);
 
     }
 }
